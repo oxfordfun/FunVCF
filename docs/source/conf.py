bda0247224c67468a5b13fa0680f3878c40952e7
oxfordfun <fan.yang.oxford@gmail.com>
2019-10-06 19:39:35 +0100

    Add sphinx_rtd_theme

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -30,7 +30,8 @@ release = '0.0.1'
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
-extensions = ['sphinx.ext.autodoc'
+extensions = [
+    "sphinx_rtd_theme"
 ]
 
 # Add any paths that contain templates here, relative to this directory.
@@ -47,7 +48,7 @@ exclude_patterns = []
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = 'alabaster'
+html_theme = 'sphinx_rtd_theme'
 
 master_doc = 'index'
 
