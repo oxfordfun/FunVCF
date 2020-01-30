b2fddd91887ac2d9116daf4e24f867d8640ba47c
Fan Yang <fan.yang-turner@ndm.ox.ac.uk>
2020-01-30 16:52:12 +0000

    update output

--- bin/vcf-compare.py
+++ bin/vcf-compare.py
@@ -4,6 +4,7 @@ import os
 import csv
 import time
 import argparse
+import json
 from pathlib import Path
 
 def sub_to_string(substitution):
@@ -19,9 +20,10 @@ def write_csv(rows, csv_file):
     '''
     write dictionary(rows) to file(csv_file)
     '''
-    #header = 'POS', 'REF', 'ALT1', 'ALT2'
+    header = 'POS', 'REF', 'ALT1', 'ALT2'
     with open(csv_file, 'w') as f:
         w = csv.writer(f)
+        w.writerow(header)
         for row in rows:
             w.writerow(row)
     
@@ -48,14 +50,11 @@ def compareVCFs(vcf_1, vcf_2):
     '''
     pos1, ref1, alt1 = readVCF(vcf_1)
     pos2, ref2, alt2 = readVCF(vcf_2)
-  
+
     all_pos = sorted(pos1 | pos2)
     both_pos = pos1 & pos2
-    diff1_2 = pos1.difference(pos2)
-    diff2_1 = pos2.difference(pos1)
     sym_diff = pos1 ^ pos2
-
-    print(f'Pos1: {len(pos1)} --Pos2: {len(pos2)} --All Pos:{len(all_pos)} --Intersect: {len(both_pos)} -- Diff1: {len(diff1_2)} --Diff2: {len(diff2_1)} --symmatric diff: {len(sym_diff)}' )
+    
     diff_all = {}
     for pos in all_pos:
         if pos in sym_diff: # report all symmatric differences
@@ -68,7 +67,16 @@ def compareVCFs(vcf_1, vcf_2):
             if alt1[str(pos)] != alt2[str(pos)]:
                 alt = ref1[str(pos)], alt1[str(pos)], alt2[str(pos)]
                 diff_all[str(pos)] = alt
-    return diff_all
+    
+    stats = {}  
+    stats['all_pos']  = len(all_pos)
+    stats['both_pos'] = len(both_pos)
+    stats['diff1_2']  = len(pos1.difference(pos2))
+    stats['diff2_1']  = len(pos2.difference(pos1))
+    stats['sym_diff'] = len(sym_diff)
+    stats['all_diff'] = len(diff_all)
+
+    return diff_all, stats
 
 
 if __name__ == "__main__":
@@ -82,12 +90,11 @@ if __name__ == "__main__":
     parser.add_argument("-o", dest="output", required=True, help="output files")
 
     args = parser.parse_args()
-    diff_all = compareVCFs(args.vcf_1, args.vcf_2)
-    print(f'Diff All: {len(diff_all)}')
+    diff_all, stats = compareVCFs(args.vcf_1, args.vcf_2)
+    print(json.dumps(stats))
 
     rows = []
     for key in diff_all:
         row = key, diff_all[key][0], diff_all[key][1], diff_all[key][2]
-        print(row)
         rows.append(row)
     write_csv(rows,args.output)
